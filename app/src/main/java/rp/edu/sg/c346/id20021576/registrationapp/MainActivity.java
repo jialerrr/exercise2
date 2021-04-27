2cf6e0a497964703311425045e04292ac6f02e0f
jialer <20021576@myrp.edu.sg>
2021-04-27 15:38:02 +0800

    Initial commit

---
+++ app/src/main/java/rp/edu/sg/c346/id20021576/registrationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package rp.edu.sg.c346.id20021576.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise2);
+    }
+}
